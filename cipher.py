c22da1740d88708dd3a0235451d1d84da3ac3906
keixjk <kayegallogo@gmail.com>
2023-04-22 06:47:21 +0800

    ask user for input

---
+++ cipher.py
@@ -0,0 +1,10 @@
+# Pseudocode
+# Get input from user
+plaintext = input("Enter plaintext (all uppercase letters, no spaces): ")
+keyword = input("Enter keyword (all uppercase letters): ")
+# Function to convert keyword to numerical values
+# Function to encrypt plaintext using Vigenère cipher
+ # Convert keyword to numerical values
+# Convert plaintext to numerical values
+# Encrypt plaintext
+# Generate Vigenère cipher and print result
