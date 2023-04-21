76285e8d0e5bcf35da3cf073c20816dd965f1203
keixjk <kayegallogo@gmail.com>
2023-04-22 06:49:28 +0800

    convert input keyword to numerical value

--- cipher.py
+++ cipher.py
@@ -3,6 +3,8 @@
 plaintext = input("Enter plaintext (all uppercase letters, no spaces): ")
 keyword = input("Enter keyword (all uppercase letters): ")
 # Function to convert keyword to numerical values
+def key_to_num(keyword):
+    return [ord(char) - 65 for char in keyword]
 # Function to encrypt plaintext using Vigenère cipher
  # Convert keyword to numerical values
 # Convert plaintext to numerical values
